13273fed0a97e4f055b1c0b78c4cd7c1a8c98d3a
Q-Mart <qumarthjash@gmail.com>
2018-09-25 16:21:13 +0100

    Init commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+
+# TODO move this to cfg
+PORT = 8888
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
+
+    def check_origin(self, origin):
+        return True
+
+
+def make_app():
+    root = os.path.dirname(os.path.abspath(__file__))
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/resources/(.*)", tornado.web.StaticFileHandler, {"path": root}),
+        (r"/websocket", EchoWebSocket)
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(PORT)
+    print("Now serving on port {0}".format(PORT))
+    tornado.ioloop.IOLoop.current().start()
