a0fd5fde629c21123452ff12342925eb2b9c251e
Q-Mart <qumarthjash@gmail.com>
2018-12-22 14:06:42 +0000

    Less broken numbers solver

--- carol/numbers.py
+++ carol/numbers.py
@@ -1,34 +1,11 @@
 import itertools
-import astar
 import collections
-import operator
 
-State = collections.namedtuple('State', ['currentVal', 'numbersLeft', 'lastOperation'])
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.children = collections.defaultdict(list)
 
-def solveNumbersRound(goal, numbers):
-
-    goalFunc = lambda x: x.currentVal == goal
-    hFunc = lambda x: goal - x.currentVal
-    gFunc = hFunc
-
-    def children(s):
-        operators = [('+', operator.add),
-                     ('-', operator.sub),
-                     ('*', operator.mul),
-                     ('/', operator.truediv)]
-        kids = []
-        for n in s.numbersLeft:
-            for ch, op in operators:
-                i = s.numbersLeft.index(n)
-                newNumbersLeft = s.numbersLeft[i+1:] + s.numbersLeft[:i]
-                newCurrentVal = op(s.currentVal, n)
-
-                kids.append(State(newCurrentVal, newNumbersLeft, ch))
-
-        return kids
-
-    searcher = astar.Astar(State(0, numbers, None), goalFunc, children, hFunc, gFunc)
-    return searcher.executeSearch()
 
 def allResultsFrom(a,b):
     yield '+', a + b
@@ -47,81 +24,58 @@ def pairsAndRejects(numbers):
         del rejects[rejects.index(pair[0])]
         del rejects[rejects.index(pair[1])]
 
-        yield pair, tuple(rejects)
+        yield pair, rejects
 
-def findAllPaths(graph, start, end, path=[]):
-    path = path + [start]
-    if start == end:
-        return [path]
-    if start not in graph.keys():
-        return []
-    paths = []
-    for node in graph[start]:
-        if node not in path:
-            newpaths = findAllPaths(graph, node, end, path)
-            for newpath in newpaths:
-                paths.append(newpath)
-    return paths
+def generateTree(numbers, target, t=None):
+    if t==None: t=Node(tuple(sorted(numbers)))
+    # base case
+    if len(numbers) == 1: return t
+    if target in numbers: return t
 
-def solve(numbers, target):
-    numbers = tuple(sorted(numbers))
-    graph = {}
+    for pair, rejects in pairsAndRejects(numbers):
+        a, b = pair[0], pair[1]
+        for op, c in allResultsFrom(a,b):
+            newNumbers = sorted(tuple(rejects + [c]))
+            newNode = Node(newNumbers)
+            t.children[newNode].append(str(a) + op + str(b))
 
-    def r(numbers):
-        if len(numbers) == 2:
-            a, b = numbers[0], numbers[1]
-            if b / a > target or a * b < target:
-                return
-        for pair, rejects in pairsAndRejects(numbers):
-            a, b = pair[0], pair[1]
-            for op, c in allResultsFrom(a, b):
-                node = tuple(sorted(list(rejects) + [c]))
+        for child in t.children.keys():
+            generateTree(newNumbers, target, child)
 
-                if node in graph:
-                    graph[node][numbers].add(((a,b), op, c))
-                    if c == target: return
-                else:
-                    graph[node] = collections.defaultdict(set)
-                    graph[node][numbers].add(((a,b), op, c))
-                    if c == target: return
-                    r(node)
+    return t
 
-    for i in range(len(numbers)):
-        for comb in itertools.combinations(numbers, i+1):
-            r(comb)
+def findPath(target, root):
+    visited = set()
+    stack = [(root, [])]
 
-    return graph
+    while stack:
+        (node, path) = stack.pop()
+        if node.value == [15, 50]:
+            for c, op in node.children.items():
+                print(node.value, c.value, op)
 
-def prettyPrint(path, graph):
-    for node in path:
-        if node in graph:
-            print(node, graph[node])
+        if node not in visited:
+            if target in node.value:
+                return path
+            visited.add(node)
+
+            for child, operations in node.children.items():
+                stack.append((child, path + [operations[0]]))
 
 
-# r = solveNumbersRound(386.0, (25, 75, 50, 1, 9, 3))
-# print(r)
+def prettyPrint(root):
+    print (root.value)
+    for child in root.children.keys():
+        print ('\t', prettyPrint(child))
 
-# numbers = (25, 75, 50, 1, 9, 31)
-numbers = (25, 75)
-operators = ('+', '-', '*', '-')
+# nums = [5, 100, 25]
+# t = 125
+# g = generateTree(nums, t)
+# print (findPath(t, g))
 
-#     for c in itertools.combinations(numbers, i):
-#         for p in itertools.permutations(c):
-#             print(p)
-
-nums = [5, 100, 25]
-g = solve(nums, 125)
-
-paths = findAllPaths(g, (125,), tuple(sorted(nums)))
-print (paths)
-
-for k1,v1 in g.items():
-    print (k1, '\n')
-
-    for k2,v2 in v1.items():
-        print ('\t', k2, v2)
-
-for path in paths:
-    prettyPrint(path, g)
-    print('-------------------------------------------------------\n')
-
+nums = [25, 5, 3, 2]
+t = 750
+g = generateTree(nums, t)
+# for k,v in g.children.items():
+#     print (v)
+print (findPath(t, g))
